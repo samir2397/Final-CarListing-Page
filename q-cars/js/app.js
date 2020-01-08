bac60ecd3efab0eea8c594d8e9e2a3a1b9732e79
samir <51944910+samir2397@users.noreply.github.com>
2020-01-08 19:18:59 +0400

    cr-listt

--- q-cars/js/app.js
+++ q-cars/js/app.js
@@ -8,3 +8,17 @@ $(this).parents(".carSlide").find("[dt-slide="+pitIndex+"]").fadeIn(250)
 },mouseleave:function(){
     $(".pit-bot").css("border-bottom-color","white")
 }})
+
+$(function(){
+    $(".remove-bar").on("click",function(){
+      $(this).addClass("d-none")
+      $(".rmv-ico").removeClass("d-none")
+      $(".menu").removeClass("menuActive")
+    })
+    $(".rmv-ico").on("click",function(){
+      $(this).addClass("d-none")
+      $(".menu").addClass("menuActive")
+  
+      $(".remove-bar").removeClass("d-none")
+    })
+  })
